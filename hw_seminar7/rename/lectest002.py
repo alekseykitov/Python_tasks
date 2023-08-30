628886b490b652228ba14111d7e5c107d772389c
alekseykitov <126381209+alekseykitov@users.noreply.github.com>
2023-08-30 16:23:51 +0300

    seminar7 homework added

---
+++ hw_seminar7/rename/lectest002.py
@@ -0,0 +1,26 @@
+#Итераторы и генераторы
+# data = {'один': 1, 'два': 2, 'три': 3}
+# x = iter(data.items())
+# print(x)
+# y = next(x)
+# print(y)
+# z = next(iter(y))
+# print(z)
+
+# data = [1,2,3,4,5,6,7]
+# for i, num in (enumerate(data, start=2)):
+#     print(f'{i=} = {num} ' )
+
+# Модули
+
+def do_important_work():
+    func()
+
+def func():
+    pass
+
+
+# other file
+import lectures
+
+lectrues.
